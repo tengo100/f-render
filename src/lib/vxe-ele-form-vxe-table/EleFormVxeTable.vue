578b5fa2b4fc806422330de76e0983a4d9263381
renxiaoteng <tengo100@163.com>
2024-10-25 16:39:00 +0800

    增加细表增加删除行按钮

--- src/lib/vxe-ele-form-vxe-table/EleFormVxeTable.vue
+++ src/lib/vxe-ele-form-vxe-table/EleFormVxeTable.vue
@@ -6,6 +6,29 @@
     :style="desc.style"
     v-on="onEvents"
   >
+    <template #crud_header="{}">
+      <span style="cursor:pointer;color:red;" title="清空" @click="resetRow">
+        <i class="el-icon-remove-outline" />
+      </span>
+      <span
+        style="cursor:pointer;color:#00A99D;margin-left:5px;"
+        title="添加"
+        @click="addRow"
+      >
+        <i class="el-icon-circle-plus-outline" />
+      </span>
+    </template>
+    <template #crud_default="{row, rowIndex }">
+      <span style="cursor:pointer;color:red;" @click="delRow(row, rowIndex)">
+        <i class="el-icon-remove-outline" />
+      </span>
+      <span
+        style="cursor:pointer;color:#00A99D;margin-left:5px;"
+        @click="addRow"
+      >
+        <i class="el-icon-circle-plus-outline" />
+      </span>
+    </template>
   </vxe-grid>
 </template>
 
@@ -17,6 +40,34 @@ export default {
   computed: {
     computedAttrs() {
       return Object.assign(this.attrs, this.attrs.gridOption);
+    }
+  },
+  methods: {
+    async addRow() {
+      const { tableData } = this.$refs.grid.getTableData();
+      const intnum = tableData.length + 1;
+      const { row: newRow } = await this.$refs.grid.insertAt(
+        { intnum, ...this.$attrs["add-fields"] },
+        -1
+      );
+      await this.$refs.grid.setActiveRow(newRow);
+      this.$emit("addRow");
+    },
+    delRow(row, rowIndex) {
+      this.$refs.grid.remove(row);
+      this.$emit("delRow", row);
+    },
+    resetRow() {
+      this.$confirm("确定要清空数据吗?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$refs.grid.remove();
+          this.$emit("resetRow");
+        })
+        .catch(() => {});
     }
   }
 };
